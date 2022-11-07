6af694aab4bd31a4d3c0661ed1aca4eb4283582c
Isabella Lopes <icmlopes@gmail.com>
2022-11-07 14:48:52 -0300

    atualização

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
+
 app.use(cors());
+
 app.use(express.json())
 
 const tweets = []
+
 const userInfo = []
 
 app.post("/sign-up", (req, res) => {
